992281669037b7f1a5d1f4d423c8bc50366d5565
Corbin09 <phuong2003.vt@gmail.com>
2025-03-12 20:10:05 +0700

    source code

---
+++ src/main/java/Se2/MovieTicket/MovieTicketApplication.java
@@ -0,0 +1,13 @@
+package Se2.MovieTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketApplication.class, args);
+	}
+
+}
