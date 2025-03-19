8531c07d8cf085d9d61015dbfd193275ce4c8257
Leyla-la <tranthigiangpltn@gmail.com>
2025-03-19 23:43:00 +0700

    file need to handle login/auth

---
+++ src/main/java/Se2/MovieTicket/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package Se2.MovieTicket.service;
+
+import Se2.MovieTicket.model.User;
+import Se2.MovieTicket.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository; // Replace with your repository
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole())) // Thêm quyền của người dùng
+        );// Here you can set user roles or authorities as needed
+
+    }
+
+}
