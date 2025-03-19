545825cb7c8e91f16a17739d270d5d39a9287a42
Leyla-la <tranthigiangpltn@gmail.com>
2025-03-19 23:43:34 +0700

    file updated to make login process act correctly

--- src/main/java/Se2/MovieTicket/service/UserService.java
+++ src/main/java/Se2/MovieTicket/service/UserService.java
@@ -1,13 +1,21 @@
 package Se2.MovieTicket.service;
 
 import Se2.MovieTicket.dto.UserDTO;
+import Se2.MovieTicket.impl.UserDetailsImpl;
 import Se2.MovieTicket.model.User;
 import Se2.MovieTicket.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -60,5 +68,38 @@ public class UserService {
 
     public void deleteUser (Long id) {
         userRepository.deleteById(id);
+    }
+
+    public boolean hasRole(String role) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return false;
+        }
+
+        return authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(role));
+    }
+
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserDetailsImpl) {
+            Long userId = ((UserDetailsImpl) principal).getId();
+            return userRepository.findById(userId).orElse(null);
+        }
+        return null;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities(Long userId) {
+        Optional<User> currentUser = userRepository.findById(userId);
+        if (currentUser.isPresent()) {
+            return Collections.singletonList(new SimpleGrantedAuthority(currentUser.get().getRole()));
+        } else {
+            return Collections.emptyList(); // Or handle the case when the user is not found
+        }
     }
 }
