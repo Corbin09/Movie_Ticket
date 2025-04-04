ebf8abae9573df954cec6cb5bcbdd492e93b17af
Leyla-la <tranthigiangpltn@gmail.com>
2025-04-05 04:41:58 +0700

    apply favicon but nhu cek

--- src/main/java/Se2/MovieTicket/config/LocaleConfig.java
+++ src/main/java/Se2/MovieTicket/config/LocaleConfig.java
@@ -3,7 +3,7 @@ package Se2.MovieTicket.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import java.util.Locale;
@@ -13,17 +13,15 @@ public class LocaleConfig {
 
     @Bean
     public LocaleResolver localeResolver() {
-        CookieLocaleResolver resolver = new CookieLocaleResolver();
-        resolver.setDefaultLocale(Locale.ENGLISH); // Ngôn ngữ mặc định
-        resolver.setCookieName("lang"); // Lưu ngôn ngữ vào cookie
-        resolver.setCookieMaxAge(3600 * 24 * 30); // Cookie tồn tại 30 ngày
-        return resolver;
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
     }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("lang"); // Dùng param "lang" để đổi ngôn ngữ
-        return interceptor;
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); // The parameter name to change the locale
+        return lci;
     }
 }
--- src/main/java/Se2/MovieTicket/config/WebMvcConfig.java
+++ src/main/java/Se2/MovieTicket/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package Se2.MovieTicket.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,4 +25,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //        registry.addResourceHandler("/images/**")
 //                .addResourceLocations("file:/src/main/resources/static/images/news");
 //    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.noCache())
+                .resourceChain(false);
+    }
 }
