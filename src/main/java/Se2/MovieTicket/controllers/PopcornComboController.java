5ed180b406a6a3bc6b96537823e99f66073c710f
Leyla-la <tranthigiangpltn@gmail.com>
2025-04-16 19:48:31 +0700

    complete pj

--- src/main/java/Se2/MovieTicket/controllers/PopcornComboController.java
+++ src/main/java/Se2/MovieTicket/controllers/PopcornComboController.java
@@ -7,12 +7,13 @@ import Se2.MovieTicket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/popcorn-combos")
+@Controller
+@RequestMapping("/")
 public class PopcornComboController {
     @Autowired
     private PopcornComboService popcornComboService;
